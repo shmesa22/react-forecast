de95c1ea3221bc2243ca6f9afb94f4f9f16e95b3
shmesa22 <shmesa23@gmail.com>
2017-07-18 10:27:02 -0500

    Add show data

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import reducers from './reducers';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
